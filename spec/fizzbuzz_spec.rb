304bf32ae6a75f221ff9793839401258ea2e315c
Hamza Sheikh <hsheikhm@gmail.com>
2015-10-08 11:25:43 +0100

    added test case for multiple of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,11 +7,17 @@ describe 'fizzbuzz' do
   it 'return the given number if it is not multiple of 3' do
     expect(fizzbuzz(11)).to eq 11
   end
+
   it 'returns the given number if number is not equal to 3' do
      expect(fizzbuzz(2)).to eq 2
    end
 
   it 'returns "fizz" if it is multiple of 3' do
-    expect(fizzbuzz(6)).to eq 'fizz' 
+    expect(fizzbuzz(6)).to eq 'fizz'
   end
+
+  it 'returns "Buzz" if it is a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+
 end
